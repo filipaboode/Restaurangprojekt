8f8d2bd1dd5a1fd33dd03d08cf66ecd50b8af04f
filipaboode <filip.aboode1@hotmail.com>
2025-03-20 21:26:38 +0100

    First commit

---
+++ Controllers/Meny.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restaurangprojekt.Controllers
+{
+    public class Meny : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿let bookingModal = document.getElementById("bookingModal");
+let bookTableBtn = document.querySelector(".hero button"); // knappen i hero sektionen
+let closeBtn = document.querySelector(".close-btn");
+
+//När man klickar på "Boka bord" öppnas modalen
+bookTableBtn.addEventListener("click", function () {
+    bookingModal.style.display = "block";
+});
+
+// När man trycker på X
+closeBtn.addEventListener("click", function () {
+    bookingModal.style.display = "none";
+});
+
+//stänger modalen om man klickar utanför 
+window.addEventListener("click", function (event) {
+    if (event.target === bookingModal) {
+        bookingModal.style.display = "none";
+    }
+});
